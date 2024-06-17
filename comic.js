fc54a950311783290102a0954a4f957aa5cd8c81
poinka <pkorobejnikova18@gmail.com>
2024-06-17 22:21:12 +0300

    Start working on comic.js

--- comic.js
+++ comic.js
@@ -1,8 +1,15 @@
-let email = "p.korobeinikova@innopolis.university";
-let data = await fetch("https://fwd.innopolis.university/api/hw2")
+document.addEventListener('DOMContentLoaded', async () => {
+    const email = 'p.korobeinikova@innopolis.university'; // Replace with actual email input method
+    const urlParams = new URLSearchParams({ email });
+    const comicIdUrl = `https://fwd.innopolis.university/api/hw2?${urlParams.toString()}`;
 
-if (data.ok) {
-    let json = await data.json();
-} else {
-    alert("Error: " + data.status);
-}
+    const id = await fetch(comicIdUrl);
+    if (id.ok) {
+        const url = `https://fwd.innopolis.university/api/comic?id=${id.text}`;
+        const data = await fetch(url);
+        console.log(data);
+    } else {
+        alert("Error: " + id.status);
+    }
+})
+
