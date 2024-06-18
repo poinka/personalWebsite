07d17ed0316f7a4a89864f4f25df3dffc3288a58
poinka <pkorobejnikova18@gmail.com>
2024-06-18 16:36:18 +0300

    Working comic.js

--- comic.js
+++ comic.js
@@ -1,15 +1,45 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const email = 'p.korobeinikova@innopolis.university'; // Replace with actual email input method
-    const urlParams = new URLSearchParams({ email });
-    const comicIdUrl = `https://fwd.innopolis.university/api/hw2?${urlParams.toString()}`;
+    const email = 'p.korobeinikova@innopolis.university'; 
+    const emailParam = new URLSearchParams({email : email});
+    const urlId = `https://fwd.innopolis.university/api/hw2?${emailParam.toString()}`;
+    
+    //console.log(urlParams.toString());
 
-    const id = await fetch(comicIdUrl);
-    if (id.ok) {
-        const url = `https://fwd.innopolis.university/api/comic?id=${id.text}`;
-        const data = await fetch(url);
-        console.log(data);
+    const idResponse = await fetch(urlId);
+
+    if (idResponse.ok) {
+        const comicId = await idResponse.text();
+        const idParam = new URLSearchParams({id : comicId});
+        const url = `https://fwd.innopolis.university/api/comic?${idParam.toString()}`;
+        
+        const comicResponse = await fetch(url);
+        if (comicResponse.ok) {
+            const data = await comicResponse.json();
+            
+            const comicContainer1 = document.getElementById("comic-container-line");
+
+            const titleElement = document.createElement('h2');
+            titleElement.textContent = `Title: ${data.safe_title}`; 
+            comicContainer1.appendChild(titleElement);
+
+            const dateElement = document.createElement('h2');
+            dateElement.innerHTML = `Date: ${new Date(data.time).toLocaleDateString()}`;
+            comicContainer1.appendChild(dateElement);
+
+            const imgElement = document.createElement('img');
+            imgElement.src = data.img;
+            imgElement.alt = data.alt;
+            comicContainer1.appendChild(imgElement);
+
+            const comicContainer2 = document.getElementById("comic-container-block2");
+
+            
+            
+        } else {
+            alert("Error fetching comic: " + comicResponse.status);
+        }
     } else {
-        alert("Error: " + id.status);
+        alert("Error finding ID: " + idResponse.status);
     }
 })
 
