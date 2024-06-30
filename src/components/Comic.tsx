a3f39dc1cf0e2d4b5e4fc63404fa0c051ac061f0
poinka <pkorobejnikova18@gmail.com>
2024-06-30 14:54:59 +0300

    feat: All stuff

---
+++ src/components/Comic.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import axios from "axios";
+
+interface IComic {
+    safe_title: string;
+    img: string;
+    alt: string;
+    year: number; 
+    month: number; 
+    day: number;
+}
+
+class Comic extends React.Component {
+    state = {
+        comic: null as IComic | null,
+    };
+
+    fetchComic = async () => {
+        const email: string = "p.korobeinikova@innopolis.university";
+        const emailParam: URLSearchParams = new URLSearchParams({ email });
+        const urlId: string = `https://fwd.innopolis.university/api/hw2?${emailParam.toString()}`;
+        axios.get(urlId).then((comicId) => {
+            const url: string = `https://fwd.innopolis.university/api/comic?id=${comicId.data}`;
+
+            axios.get(url).then((data) => {
+                this.setState({ comic: data.data });
+            })
+        });
+        
+        
+        
+    }
+
+    render() {
+        this.fetchComic();
+        const { comic } = this.state;
+
+        if (!comic) {
+            return <div>Loading...</div>;
+        }
+        const date = new Date(comic.year, comic.month-1, comic.day);
+        return (
+            <div className="block">
+                <h2>Title: {comic.safe_title}</h2>
+                <img src={comic.img} alt={comic.alt}></img>
+                <h2>Date: {date.toLocaleDateString()}</h2>
+            </div>
+        );
+    }
+}
+
+export default Comic;
