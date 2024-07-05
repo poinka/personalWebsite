95147c53b47b7991ea3317d180b385bd42890ca7
poinka <pkorobejnikova18@gmail.com>
2024-07-05 12:24:01 +0300

    image fix

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
           <div>
             <Image
               className="main-photo"
-              src="/mainPhoto.png"
+              src="mainPhoto.png"
               alt="Main photo"
               width={1000}
               height={1000}
