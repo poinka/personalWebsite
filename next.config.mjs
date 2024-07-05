7ad2a57804cb0d231c2d5b78be27ccc14b505aea
poinka <pkorobejnikova18@gmail.com>
2024-07-05 18:55:06 +0300

    feat: update github action

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
-export default {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   output: "export",
-  reactStrictMode: true,
-  basePath: "/personalWebsite",
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
+export default nextConfig;
