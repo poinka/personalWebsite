c25c7973fc296034d8a4851956c4a8cf986c0014
poinka <pkorobejnikova18@gmail.com>
2024-07-06 12:15:35 +0300

    add assetrefix

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@
  */
 const nextConfig = {
   output: "export",
+  assetPrefix: "/personalWebsite/",
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
